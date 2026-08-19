b708d2e50bd0c86b407e8058ab1838c9f86c1928
Jhon leo Ronquillo <jhle.ronquillo.au@phinmaed.com>
2026-08-19 12:07:26 +0800

    Add a program to sum three user-input numbers

---
+++ main.py
@@ -0,0 +1,7 @@
+L = int(input("Enter the first number: "))
+E = int(input("Enter the second number: "))
+O = int(input("Enter the third number: "))
+
+total = L + E + O
+
+print("The sum of the three numbers is:", total)
